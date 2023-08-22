ee2bfca8eded03d4db1d28c08894ca7429100d07
usuario <usuario@email.com>
2023-08-21 21:46:46 -0300

    Ajustes

--- logica-de-programacao/index.js
+++ logica-de-programacao/index.js
@@ -42,3 +42,16 @@ if (num1 === 0) {
 } else {
   console.log("Número ímpar");
 }
+
+/**
+ * Exercícios
+ * Escreva um programa que verifique se um número é positivo, negativo ou zero.
+ * Escreva um programa que determine se um número é par ou ímpar.
+ * Escreva um programa que verifique se um ano é bissexto. 
+   Um ano é bissexto se for divisível por 4, mas não por 100. 
+   No entanto, anos divisíveis por 400 também são bissextos.
+ * Escreva um programa que calcule o IMC (Índice de Massa Corporal) 
+   de uma pessoa com base em seu peso e altura. 
+   O programa deve exibir uma mensagem informando se a pessoa 
+   está abaixo do peso, com peso normal, com sobrepeso ou obesa.
+ */
