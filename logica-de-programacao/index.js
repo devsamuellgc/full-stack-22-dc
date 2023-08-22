ea80692d49f1091244aa43acbf699eab27977561
usuario <usuario@email.com>
2023-08-21 21:44:24 -0300

    Começo de lógica

---
+++ logica-de-programacao/index.js
@@ -0,0 +1,44 @@
+// const nome = "Samuel";
+// let valor = "1";
+// console.log(typeof "Samuel");
+// console.log(typeof "1");
+// console.log(typeof 1);
+// console.log(typeof false);
+// console.log("1" == 1);
+// console.log("1" === 1);
+//             0  1  2  3  4  5     6        7
+// const array = [1, 2, 3, 4, 5, 6, "samuel", "teste"];
+// console.log(array);
+
+/**
+ * 1º Escreva um programa que verifique 
+    se um número é positivo, negativo ou zero.
+ * 2º Escreva um programa que determine 
+    se um número é par ou ímpar.
+ */
+let num1 = 2;
+let num2 = 5;
+
+if (condicao) {
+  // se condição for verdadeira
+} else if (condicao2) {
+  // se a condição um não for verdadeira e a condição dois sim
+} else {
+  // se condição for falsa
+}
+
+if (num1 === 0) {
+  console.log("Número é zero");
+} else if (num1 > 0) {
+  console.log("Número é positivo");
+} else {
+  console.log("Número negativo");
+}
+
+if (num1 === 0) {
+  console.log("Número 0");
+} else if (num1 % 2 === 0) {
+  console.log("Número par");
+} else {
+  console.log("Número ímpar");
+}
